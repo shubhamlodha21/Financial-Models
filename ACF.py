bccb9f71537e93d80588580195f428226337235c
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2025-12-28 21:11:44 +0530

    Update Tesla data download end date to 2025

--- ACF.py
+++ ACF.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot
 from statsmodels.graphics.tsaplots import plot_acf
 import yfinance as yf
-tesla = yf.download('TSLA','2019-01-27', '2020-02-11')
+tesla = yf.download('TSLA','2019-01-27', '2025-02-11')
 plot_acf(tesla['Close'], lags=20)
 pyplot.show()
